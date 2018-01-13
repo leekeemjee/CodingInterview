a108dad7e84f89f17e721e3e62f6068c81c7b830
Leekeemjee <Leekeemjee@gmail.com>
2018-01-13 17:02:29 +0700

    Add project files.

---
+++ InterviewQuestion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        // Question 1: 
+        // Assume you have a method isSubstring which checks if one word is a substring of
+        // another.Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using
+        // only one call to isSubstring(i.e., “waterbottle” is a rotation of “erbottlewat”).
+    }
+}
